5b658b6883a2a42e3f4a8dafab793cde21bbc7a9
CRGdeveloper <114066596+CRGdeveloper@users.noreply.github.com>
2023-02-07 13:30:46 +0530

    changes

--- src/components/Celebrity/AnupamKher.js
+++ src/components/Celebrity/AnupamKher.js
@@ -7,7 +7,7 @@ import SliderParent from "../Slider";
 import { HomeSlider } from "../../constant/homeSilder";
 import Card from "../NftCard/card";
 import SliderSection from "../SectionCard/slider-section";
-import { Link, NavLink, useNavigate } from "react-router-dom";
+import { Link, NavLink, useNavigate, useParams } from "react-router-dom";
 import { SetpopupReducerData } from "../../store/reducer";
 import { useWeb3 } from "@3rdweb/hooks";
 import OpenModal from "../Navbar/OpenModal";
@@ -16,17 +16,22 @@ import CreatePopUp from "../CreateScreen/CreatePopUp";
 import Wallet from "../Navbar/Wallet";
 import Offcanvas from "react-bootstrap/Offcanvas";
 import Subscribe from "../../pages/Subscribe";
+import { API } from "../../apiwrapper";
+import { apiURl } from "../../store/actions";
 
 function AnupamKher() {
   const { CuratedNft, TrendingNft, bipoc, femalecreator, lgbtq } = useSelector(
     (state) => state.Slider
   );
+  const { Creatorname, Slug } = useParams()
   const [text, setText] = useState("");
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const { address, disconnectWallet, error } = useWeb3();
   const [show, setShow] = useState(false);
   const [showPopup, setShowPopup] = useState(false);
+  const [categoryList, setcategoryList] = useState([]);
+  const [NftListAccording, setNftListAccording] = useState([])
 
   const { loginUserData = {} } = useSelector((state) => state.authUser);
   const { User } = useSelector((state) => state);
@@ -39,6 +44,33 @@ function AnupamKher() {
     dispatch(SetpopupReducerData({ modalType: "LOGIN", showModal: true }));
     setShowPopup(true);
   };
+
+  useEffect(() => {
+    API({ url: `${apiURl.categorydata}/${Slug}`, method: 'GET' }).then((data) => data).then((data) => {
+      if (data.status) {
+        setcategoryList(data.data?.allCategory || [])
+      }
+    })
+  }, [Slug]);
+
+  useEffect(() => {
+    if (categoryList.length) {
+      Promise.all(
+        categoryList.map((value) => API({ url: `${apiURl.GetCollections}/${Slug}/${value._id}`, method: 'GET' }).then((data) => data))
+      ).then((values) => {
+        let dataArray = [];
+        values.map((data) => {
+          dataArray[data.collection_id] = Array.isArray(data.response) ? data.response : []
+        });
+
+        setNftListAccording(dataArray);
+
+      })
+    }
+  }, [Slug, categoryList])
+
+
+
   const handleShowCreatePopup = (e) => {
     console.log(e);
     e.preventDefault();
@@ -146,95 +178,105 @@ function AnupamKher() {
         </div>
       </section>
 
-      <section className="comanNftSec coffeeLigh">
-        <div className="container">
-          <div className="row">
-            <div className="col-xl-12">
-              <h2>Exclusive Collection</h2>
+      {/* all categoery edition come by celebrity */}
 
-              <SliderParent
-                className={`top`}
-                autoplay={true}
-                draggable={true}
-                arrows={true}
-                dots={false}
-                infinite={false}
-                speed={300}
-                swipeToSlide={true}
-                slidesToShow={4}
-                slidesToScroll={4}
-                responsive={[
-                  {
-                    breakpoint: 1113,
-                    settings: {
-                      slidesToShow: 3,
-                      slidesToScroll: 3,
-                      infinite: true,
-                      dots: true,
-                    },
-                  },
-                  {
-                    breakpoint: 1113,
-                    settings: {
-                      slidesToShow: 3,
-                      slidesToScroll: 3,
-                      infinite: true,
-                      dots: true,
-                    },
-                  },
-                  {
-                    breakpoint: 1024,
-                    settings: {
-                      slidesToShow: 2,
-                      slidesToScroll: 2,
-                      infinite: true,
-                      dots: true,
-                    },
-                  },
-                  {
-                    breakpoint: 600,
-                    settings: {
-                      slidesToShow: 1,
-                      slidesToScroll: 1,
-                    },
-                  },
-                  {
-                    breakpoint: 480,
-                    settings: {
-                      slidesToShow: 1,
-                      slidesToScroll: 1,
-                    },
-                  },
-                  // You can unslick at a given breakpoint now by adding:
-                  // settings: "unslick"
-                  // instead of a settings object
-                ]}
-              >
-                {Array.isArray(TrendingNft) && TrendingNft.length ? (
-                  TrendingNft.map((value, index) => (
-                    <Card key={index} {...value} />
-                  ))
-                ) : (
-                  <div className="no-details">
-                    <img
-                      src="https://nft.crosstower.com/assets/no_data.png"
-                      className="img-responsive"
-                      alt=""
-                      width={`100%`}
-                    />
-                  </div>
-                )}
-              </SliderParent>
+      {categoryList.map((item) => {
 
-              <div className="text-center mt-4">
-                <a className="viewMore">View More</a>
+
+
+
+        return (<section className="comanNftSec coffeeLigh">
+          <div className="container">
+            <div className="row">
+              <div className="col-xl-12">
+                <h2>{item?.Categoryname}</h2>
+
+                <SliderParent
+                  className={`top`}
+                  autoplay={true}
+                  draggable={true}
+                  arrows={true}
+                  dots={false}
+                  infinite={false}
+                  speed={300}
+                  swipeToSlide={true}
+                  slidesToShow={4}
+                  slidesToScroll={4}
+                  responsive={[
+                    {
+                      breakpoint: 1113,
+                      settings: {
+                        slidesToShow: 3,
+                        slidesToScroll: 3,
+                        infinite: true,
+                        dots: true,
+                      },
+                    },
+                    {
+                      breakpoint: 1113,
+                      settings: {
+                        slidesToShow: 3,
+                        slidesToScroll: 3,
+                        infinite: true,
+                        dots: true,
+                      },
+                    },
+                    {
+                      breakpoint: 1024,
+                      settings: {
+                        slidesToShow: 2,
+                        slidesToScroll: 2,
+                        infinite: true,
+                        dots: true,
+                      },
+                    },
+                    {
+                      breakpoint: 600,
+                      settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1,
+                      },
+                    },
+                    {
+                      breakpoint: 480,
+                      settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1,
+                      },
+                    },
+                    // You can unslick at a given breakpoint now by adding:
+                    // settings: "unslick"
+                    // instead of a settings object
+                  ]}
+                >
+                  {Array.isArray(NftListAccording[item?._id]) && NftListAccording[item?._id].length ? (
+                    NftListAccording[item?._id].map((value, index) => (
+                      <Card key={index} {...value} />
+                    ))
+                  ) : (
+                    <div className="no-details">
+                      <img
+                        src="https://nft.crosstower.com/assets/no_data.png"
+                        className="img-responsive"
+                        alt=""
+                        width={`100%`}
+                      />
+                    </div>
+                  )}
+                </SliderParent>
+
+                <div className="text-center mt-4">
+                  <a className="viewMore">View More</a>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-      </section>
+        </section>
+        )
+      })}
 
-      <section className="comanNftSec">
+
+      {/* <section className="comanNftSec">
         <div className="container">
           <div className="row">
             <div className="col-xl-12">
@@ -494,7 +536,7 @@ function AnupamKher() {
             </div>
           </div>
         </div>
-      </section>
+      </section> */}
       <Subscribe />
     </div>
   );
