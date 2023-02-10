529aae93b4db27a76930f8bf8d6c6eefe5a4f142
Vijay Singh <vijaysingh@anky-Mac-mini-SS.local>
2023-02-10 19:20:44 +0530

    buyMultiXRPstripePayment

--- src/components/PopUp/Sell.js
+++ src/components/PopUp/Sell.js
@@ -55,7 +55,7 @@ const handleContinue=()=>{
               </div>
             
                 <button className="login-screen-button" type="submit" onClick={handleContinue}>
-                  Continue
+                  Close
                 </button>
              
             </div>
