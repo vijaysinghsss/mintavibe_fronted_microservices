f5b198dd164b4feb60e91e9100b1696c0bb7f2e8
CRGdeveloper <114066596+CRGdeveloper@users.noreply.github.com>
2023-02-15 12:13:42 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -2,13 +2,11 @@ import "react-toastify/dist/ReactToastify.css";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { BrowserRouter, Routes, Route, Navigate, ScrollRestoration } from "react-router-dom";
-
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Layout from "./pages/layout";
 import io from "socket.io-client";
 import Home from "./pages/home";
 import AboutUs from "./pages/about-us";
-import Blogs from "./pages/blogs";
 import Faq from "./pages/faq";
 import Artist from "./pages/collect-artist";
 import FormPage from "./components/CreateScreen/FormPage";
@@ -23,15 +21,11 @@ import { API } from "./apiwrapper";
 import { apiURl } from "./store/actions";
 import { SetSliderData, SetSocketData, SetUserData } from "./store/reducer";
 import ProfileSetting from "./components/ProfileSetting/ProfileSetting";
-// import 'react-select/dist/css/react-select.css';
 import PrivacyPolicy from "./pages/privacy-policy";
 import NftDetails from "./components/Profile/nft-details";
 import LoaderComponent from "./components/LoaderComponent";
 import Nftforall from "./components/NftForAll/Nftforall.js";
-
 import ContactUs from "./components/ContactUs/ContactUs";
-import { faL } from "@fortawesome/free-solid-svg-icons";
-import SubscriptionPopup from "./components/PopUp/SubscriptionPopup";
 import "./App.css";
 import "./responsive.css";
 import BlogPage from "./pages/BlogPage";
@@ -42,13 +36,9 @@ import UserProfile from "./components/UserProfile";
 const socket = io.connect(process.env.REACT_APP_BACKENDURL);
 
 function App() {
-  const { address, provider, activeProvider } = useWeb3();
+  const { address, provider } = useWeb3();
+
   const { loginUserData = null } = useSelector((state) => state.authUser);
-  const { SubscriptionUser } = useSelector((state) => state);
-
-  const { Theme } = useSelector((state) => state);
-
-  const [isMounted, setIsMounted] = useState(false);
 
   const dispatch = useDispatch();
 
@@ -153,25 +143,10 @@ function App() {
 
   useEffect(() => {
     fetchNormalData();
-    setIsMounted(true);
-    return () => {
-      setIsMounted(false);
-    };
   }, []);
 
   return (
     <BrowserRouter>
-      {/* {isMounted &&
-      <ScrollRestoration />
-        !Object.values(loginUserData).length &&
-        !Object.values(SubscriptionUser?.data).length && (
-          <SubscriptionPopup
-            isMounted={isMounted}
-            setIsMounted={setIsMounted}
-          />
-        )} */}
-
-
       <LoaderComponent />
       <ToastContainer position="top-right" />
       <Routes>
